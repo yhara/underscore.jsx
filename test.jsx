7f9ce643a4d686ff7402c21d52268533754845b1
Yutaka HARA <yutaka.hara.gmail.com>
2012-09-25 20:28:25 +0900

    Add tests

--- test.jsx
+++ test.jsx
@@ -5,12 +5,24 @@ import _ from 'underscore.jsx';
 final class _Test extends TestCase {
   override function beforeClass(tests:string[]): void {
     var eval = js.global["eval"] as (string) -> Object;
-    eval('global._ = require("/path/to/underscore-min.js")');
+    eval('global._ = require("/Users/yhara/proj/puyo/jsx/underscore.jsx/underscore-min.js")');
 
     super.beforeClass(tests);
   }
 
-  function testRange1(): void{
+  function testZip(): void{
+    this.expect(_.zip([1,2], [3,4]).toString()).toBe([[1,3],[2,4]].toString());
+  }
+
+  function testIndexOf(): void{
+    this.expect(_.indexOf(["a","b","c"], "b")).toBe(1);
+    this.expect(_.indexOf(["a","b","c"], "b", true)).toBe(1);
+    this.expect(_.lastIndexOf(["a","b","c","b"], "b")).toBe(3);
+  }
+
+  function testRange(): void{
     this.expect(_.range(3).toString()).toBe([0,1,2].toString());
+    this.expect(_.range(3,4).toString()).toBe([3].toString());
+    this.expect(_.range(3,9,2).toString()).toBe([3,5,7].toString());
   }
 }
