39ad0aca033835289ed70a91a0c016ce56bead24
Yutaka HARA <yutaka.hara.gmail.com>
2012-09-25 20:06:08 +0900

    fixed unit test

--- test.jsx
+++ test.jsx
@@ -1,21 +1,16 @@
-import "js.jsx"; // for js.global
 import 'test-case.jsx';
+import "js.jsx"; // for js.global
 import _ from 'underscore.jsx';
-
-class node {
-  static function require(name: string): Object {
-    var eval = js.global["eval"] as (string) -> Object;
-    return eval('require("'+name+'")');
-  }
-}
 
 final class _Test extends TestCase {
   override function beforeClass(tests:string[]): void {
-    var _ = node.require("./underscore.js");
+    var eval = js.global["eval"] as (string) -> Object;
+    eval('global._ = require("/path/to/underscore-min.js")');
+
     super.beforeClass(tests);
   }
 
   function testRange1(): void{
-    this.expect(_.range(3)).toBe([0,1,2]);
+    this.expect(_.range(3).toString()).toBe([0,1,2].toString());
   }
 }
