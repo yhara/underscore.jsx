a1355f466d7b776b1565a8f554449a99adf2a520
Yutaka HARA <yutaka.hara.gmail.com>
2012-09-28 01:36:22 +0900

    added signatures of map, uniq, flatten

--- underscore.jsx
+++ underscore.jsx
@@ -9,6 +9,21 @@ native class _ {
   static function range(n:number): number[];
   static function range(start:number, stop:number): number[];
   static function range(start:number, stop:number, step:number): number[];
+
+  static function flatten(array:variant): variant[];
+  static function flatten(array:variant, shallow:boolean): variant[];
+
+  static function map(obj:variant, iterator:variant): variant;
+  static function map(obj:variant, iterator:variant, context:variant): variant[];
+  static function collect(obj:variant, iterator:variant): variant;
+  static function collect(obj:variant, iterator:variant, context:variant): variant[];
+
+  static function uniq(array:variant): variant[];
+  static function uniq(array:variant, isSorted:boolean): variant[];
+  static function uniq(array:variant, isSorted:boolean, iterator:variant): variant[];
+  static function unique(array:variant): variant[];
+  static function unique(array:variant, isSorted:boolean): variant[];
+  static function unique(array:variant, isSorted:boolean, iterator:variant): variant[];
 }
 
 
